9afabca351179e0b9254e69a205341ca70cd9a5f
jodyneckles <jody@technative.tv>
2018-11-22 00:27:20 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,17 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    BRANDS.push(brand).uniq!
   end
+
+  def brand
+    @brand
+  end
+
 
   def cobble
     self.condition = "new"
